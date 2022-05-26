a41b425d2da14082fc0867355016efc503b3b9ea
Kalyani Kenekar <82832370+Kalken12@users.noreply.github.com>
2022-05-26 23:28:13 +0530

    stopwatch using useEffect react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+
+import './App.css';
+import axios from "axios"
+import Todo from './components/Todo'
+import Timer from './components/Timer'
+import Stopwatch from './components/Stopwatch';
+
+function App() {
+
+  return (
+    <div className="App">
+   
+  <Stopwatch/>
+    </div>
+  );
+}
+
+export default App;
